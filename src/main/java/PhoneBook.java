f3ed7aaad69f6ab0097f6a478c19c849e1909ab7
Shaa test <shvyrev.anton78@gmail.com>
2024-01-30 20:31:43 +0500

    add implemented

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,2 +1,24 @@
+import java.util.Map;
+import java.util.TreeMap;
+
 public class PhoneBook {
+    private  static PhoneBook instance = null;
+    private Map<String, String> book = new TreeMap<>();
+
+    private PhoneBook(){}
+
+    public static PhoneBook getInstance(){
+        if(instance == null){
+            instance = new PhoneBook();
+        }
+        return instance;
+    }
+
+    public int add(String name, String number){
+        if(book.containsKey(name)){
+            return book.size();
+        }
+        book.put(name, number);
+        return book.size();
+    }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,22 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+
 public class PhoneBookTest {
+
+    @ParameterizedTest
+    @CsvSource(value = {
+            "Petya, +79194691234, 1",
+            "Katya, +79084561234, 2",
+            "Anya, +79129463754, 3",
+            "Petya, +79123758473, 3",
+            "Masha, +79084671324, 4",
+            "Artem, +79198352946, 5",
+    })
+    void addTest(String name, String number, int result){
+        PhoneBook phoneBook = PhoneBook.getInstance();
+        int actual = phoneBook.add(name, number);
+        Assertions.assertEquals(actual, result);
+    }
 }
