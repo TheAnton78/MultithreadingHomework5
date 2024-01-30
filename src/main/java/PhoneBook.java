567132d67f30b39c94125eb9d0d39c75c9d466f0
Shaa test <shvyrev.anton78@gmail.com>
2024-01-30 21:48:37 +0500

    findByNamber implemented

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,9 +1,11 @@
+import com.google.common.collect.BiMap;
+import com.google.common.collect.HashBiMap;
 import java.util.Map;
 import java.util.TreeMap;
 
 public class PhoneBook {
     private  static PhoneBook instance = null;
-    private Map<String, String> book = new TreeMap<>();
+    public Map<String, String> book = new TreeMap<>();
 
     private PhoneBook(){}
 
@@ -23,6 +25,11 @@ public class PhoneBook {
     }
 
     public String findByNumber(String number){
-        return null;
+        BiMap<String, String> biMap = HashBiMap.create(book);
+        return biMap.inverse().get(number);
     }
+
+
+
+
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,18 +1,28 @@
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 
 public class PhoneBookTest {
 
+    @BeforeAll
+    static void beforeAll(){
+        PhoneBook phoneBook = PhoneBook.getInstance();
+        phoneBook.book.put("Vasya", "+79124688544");
+        phoneBook.book.put("Olya", "+79129678544");
+        phoneBook.book.put("Kolya", "+79194689544");
+        phoneBook.book.put("Masha", "+79084671324");
+    }
+
     @ParameterizedTest
     @CsvSource(value = {
-            "Petya, +79194691234, 1",
-            "Katya, +79084561234, 2",
-            "Anya, +79129463754, 3",
-            "Petya, +79123758473, 3",
-            "Masha, +79084671324, 4",
-            "Artem, +79198352946, 5",
+            "Petya, +79194691234, 5",
+            "Katya, +79084561234, 6",
+            "Anya, +79129463754, 7",
+            "Petya, +79123758473, 7",
+            "Masha, +79084671324, 7",
+            "Artem, +79198352946, 8",
     })
     void addTest(String name, String number, int result){
         PhoneBook phoneBook = PhoneBook.getInstance();
@@ -22,14 +32,15 @@ public class PhoneBookTest {
 
     @ParameterizedTest
     @CsvSource(value = {
-            "+79194691234, Petya",
-            "+79084561234, Katya",
+            "+79194689544, Kolya",
+            "+79129678544, Olya",
             "+79084671324, Masha",
-            "+79198352946, Artem"
+            "+79124688544, Vasya"
     })
     void findByNumberTest(String number, String name){
         PhoneBook phoneBook = PhoneBook.getInstance();
         String actual = phoneBook.findByNumber(number);
         Assertions.assertEquals(actual, name);
     }
+
 }
